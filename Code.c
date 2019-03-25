736fac28be377a6d60444706eac21b2bb90dfdfb
SrajanKumarShukla <mans50899@gmail.com>
2019-03-25 20:19:19 +0100

    Code.c

--- Code.c
+++ Code.c
@@ -4,4 +4,18 @@ int main()
 	int m;
 	printf("\nEnter number of processes: ");
 	scanf("%d",&m);
+	int a[m];
+	int b[m];
+	printf("\nEnter arival time of processes:\n");
+	for(i=0;i<m;i++)
+	{  printf("For processes %d :",i+1);
+		scanf("%d",&a[i]);
+	}
+	printf("\nEnter burst time for processes:\n");
+	for(i=0;i<m;i++)
+	{
+		printf("For processes %d:",i+1);
+		scanf("%d",&b[i]);
+		b1[i]=b[i];	
+	}
 }
