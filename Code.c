979e20368d7255e54b8722576eb98fcc870b190d
SrajanKumarShukla <mans50899@gmail.com>
2019-03-30 20:19:19 +0100

    Code.c

--- Code.c
+++ Code.c
@@ -4,8 +4,14 @@ int main()
 	int m;
 	printf("\nEnter number of processes: ");
 	scanf("%d",&m);
-	int a[m];
-	int b[m];
+	int a[m];+3
+	int b[m],b1[m];
+	int w[m];
+	int ta[m];
+	int tq1=3;
+	int tq2=6;
+	int avgT=0;
+	int avgW=0;
 	printf("\nEnter arival time of processes:\n");
 	for(i=0;i<m;i++)
 	{  printf("For processes %d :",i+1);
@@ -17,5 +23,23 @@ int main()
 		printf("For processes %d:",i+1);
 		scanf("%d",&b[i]);
 		b1[i]=b[i];	
+	}
+		printf("\n1st ITERATION \n");
+	for(i=0;i<m;i++)
+	{  
+	if(a[i]<=tq1+a[i-1])
+	{
+	
+		b[i]=b[i]-tq1;
+		
+		printf("\nFor process %d\n",i+1);
+		printf("Left Burst time=%d\n",b[i]);}
+		else
+		{
+		b[i]=b[i]-tq1;
+	
+		printf("For process %d\n",i+1);
+		printf("Left Burst time= %d\n",b[i]);	
+		}
 	}
 }
